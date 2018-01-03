444e6ecbf5b3da898c184697031aeb63c49c2dd4
Sascha Wolf <swolf.dev@gmail.com>
2018-01-03 11:20:34 +0100

    Specification - Operator: Add helper functions `clauses/1` and `rule/1`
    The functions return a result tuple containing the clauses or the rule
    atom of the given operator.
    
    In addition each function has a bang version which raises an
    `ArgumentError` in case the given argument is not an operator.
    
    Furthermore the `Operator` module now has a `operator?/1` function which
    returns a boolean if the given value is an operator.

--- lib/specification/operator.ex
+++ lib/specification/operator.ex
@@ -18,5 +18,33 @@ defmodule Specification.Operator do
     def unquote(rule)(arg1, arg2) do
       unquote(rule)([arg1, arg2])
     end
+
+    def clauses({unquote(rule), clauses}), do: {:ok, clauses}
+    def rule({unquote(rule), _clauses}), do: {:ok, unquote(rule)}
   end
+
+  def clauses(_other), do: {:error, :invalid_operator}
+  def rule(_other), do: {:error, :invalid_operator}
+
+  def clauses!(operator) do
+    case clauses(operator) do
+      {:ok, clauses} ->
+        clauses
+
+      {:error, :invalid_operator} ->
+        raise ArgumentError, "Can't extract operator clauses from: #{operator}"
+    end
+  end
+
+  def rule!(operator) do
+    case rule(operator) do
+      {:ok, rule} ->
+        rule
+
+      {:error, :invalid_operator} ->
+        raise ArgumentError, "Can't extract operator rule from: #{operator}"
+    end
+  end
+
+  def operator?(operator), do: match?({:ok, _rule}, rule(operator))
 end

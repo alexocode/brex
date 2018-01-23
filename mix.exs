257bb783e9b24adbb03075d1da7fc7ed60254de1
Sascha Wolf <swolf.dev@gmail.com>
2018-01-23 13:46:20 +0100

    Project: Compile `spec/support` in `test`

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Specification.Mixfile do
       app: :specification,
       version: "0.1.0",
       elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env()),
       preferred_cli_env: [espec: :test],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -19,6 +20,10 @@ defmodule Specification.Mixfile do
       extra_applications: [:logger]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "spec/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do

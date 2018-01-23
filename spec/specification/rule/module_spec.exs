32b05eb8fdab0f12bddd1c3179d592573561b67e
Sascha Wolf <swolf.dev@gmail.com>
2018-01-23 13:53:18 +0100

    Spec - Rules: Add the `EvaluateSpec` in the `ModuleSpec`

--- spec/specification/rule/module_spec.exs
+++ spec/specification/rule/module_spec.exs
@@ -1,15 +1,36 @@
 defmodule Specification.Rule.ModuleSpec do
   use ESpec, async: true
 
+  defmodule NotNilRule do
+    @behaviour Specification.Rule.Module
+
+    @impl Specification.Rule.Module
+    def evaluate(value) do
+      not is_nil(value)
+    end
+  end
+
+  let rule_type: described_module()
+
   it_behaves_like Shared.IsRuleSpec,
-    rule_type: described_module(),
     valid_rules: [
-      Support.SomeModuleRule
+      NotNilRule
     ],
     invalid_rules: [
       :a,
       1,
       &is_list/1,
       Specification.Operator.all([])
+    ]
+
+  it_behaves_like Shared.EvaluateSpec,
+    rule: NotNilRule,
+    valid_values: [
+      %{},
+      :a,
+      MapSet.new()
+    ],
+    invalid_values: [
+      nil
     ]
 end

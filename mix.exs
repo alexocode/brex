4b769ab041c93786f5545890e2d75b5de3e32a1c
Sascha Wolf <swolf.dev@gmail.com>
2018-02-12 19:48:20 +0100

    CI: Add a travis.yml file

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Specification.Mixfile do
     [
       app: :specification,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       preferred_cli_env: [espec: :test],
       start_permanent: Mix.env() == :prod,

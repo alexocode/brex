7bf24e4060cf0f7a71e4cb101d548468b1769ac9
Sascha Wolf <swolf.dev@gmail.com>
2018-02-13 22:35:18 +0100

    Rule - Struct: Rewrite the struct rule and add a `NestedRule`
    The `NestedRule` can, as the `Struct` rule can, be `use`d. It expects
    two keys:
    
    - `aggregator` the function to aggregate the evaluated nested rules
    - `nested` the key to the `nested_rules`
    
    Alternatively the using module itself can implement `aggregator/1`
    and/or `nested_rules/1` in case it wants more control.
    
    In turn an implementation for `Nestable` will be provided, as will an
    `evaluate/2` function which takes all nested rules, aggregates them and
    returns a result tuple based on the aggregation.

--- lib/spex/rule/struct.ex
+++ lib/spex/rule/struct.ex
@@ -17,16 +17,21 @@ defmodule Spex.Rule.Struct do
         end
       end
   """
-  alias Spex.Rule
 
   # A struct implementing this behaviour
   @type t :: struct()
 
+  defmacro __using__([]) do
+    quote do
+      @after_compile unquote(__MODULE__)
+    end
+  end
+
   defmacro __using__(opts) do
     if is_nested?(opts) do
-      use_nested(opts)
+      quote do: use(Spex.Rule.NestedStruct, unquote(opts))
     else
-      use_plain()
+      quote do: use(unquote(__MODULE__))
     end
   end
 
@@ -34,65 +39,7 @@ defmodule Spex.Rule.Struct do
     if Keyword.get(opts, :nested), do: true, else: false
   end
 
-  defp use_nested(opts) do
-    aggregator = Keyword.get(opts, :aggregator)
-    nested = Keyword.get(opts, :nested)
-
-    quote do
-      @after_compile {unquote(__MODULE__), :__defimpl_evaluable_and_nested__}
-
-      unquote(build(:aggregator, aggregator))
-      unquote(build(:nested, nested))
-    end
-  end
-
-  defp build(_, nil), do: nil
-
-  defp build(:aggregator, aggregator) when is_atom(aggregator) do
-    quote do
-      def aggregator(_rule) do
-        &apply(__MODULE__, unquote(aggregator), [&1])
-      end
-    end
-  end
-
-  defp build(:aggregator, aggregator) when is_function(aggregator, 1) do
-    quote do
-      def aggregator(_rule), do: unquote(aggregator)
-    end
-  end
-
-  defp build(:nested, true), do: nil
-
-  defp build(:nested, key) when is_atom(key) do
-    quote do
-      def nested_rules(%{unquote(key) => nested_rules}), do: nested_rules
-    end
-  end
-
-  defp build(option, value) do
-    raise ArgumentError, "Invalid value for option: #{inspect(option)}=#{inspect(value)}"
-  end
-
-  defp use_plain do
-    quote do
-      @after_compile {unquote(__MODULE__), :__defimpl_evaluable__}
-    end
-  end
-
-  def __defimpl_evaluable_and_nested__(%{module: module} = env, bytecode) do
-    __defimpl_evaluable__(env, bytecode)
-
-    defimpl_nestable(module) ||
-      raise CompileError,
-        file: env.file,
-        line: env.line,
-        description:
-          "cannot use #{inspect(__MODULE__)} with `nested` " <>
-            "on module #{inspect(module)} without defining aggregator/1 and nested_rules/1"
-  end
-
-  def __defimpl_evaluable__(%{module: module} = env, _bytecode) do
+  def __after_compile__(%{module: module} = env, _bytecode) do
     defimpl_evaluable(module) ||
       raise CompileError,
         file: env.file,
@@ -103,17 +50,8 @@ defmodule Spex.Rule.Struct do
 
   defp defimpl_evaluable(module) do
     if function_exported?(module, :evaluate, 2) do
-      defimpl Rule.Evaluate, for: module do
+      defimpl Spex.Rule.Evaluable, for: module do
         defdelegate evaluate(rule, value), to: module
-      end
-    end
-  end
-
-  defp defimpl_nestable(module) do
-    if function_exported?(module, :aggregator, 1) and function_exported?(module, :nested, 1) do
-      defimpl Rule.Nestable, for: module do
-        defdelegate aggregator(rule), to: module
-        defdelegate nested_rules(rule), to: module
       end
     end
   end

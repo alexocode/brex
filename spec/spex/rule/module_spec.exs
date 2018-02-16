39c5302a069c2ee77b6729a4f34231813aba1479
Sascha Wolf <swolf.dev@gmail.com>
2018-02-16 09:29:43 +0100

    Spec - Rule: Rewrite the ModuleSpec
    It now checks that only modules are handled properly and atoms raise an
    UndefinedFunctionError, as do modules without a `evaluate/1` function.
    
    In addition the raise and throw tests have been rewritten to no longer
    catch/rescue but instead simply crash.
    
    The thought behind this, is that a module rule is a rather deliberately
    written piece of code, compared to a function rule. If such a module
    rule raises an error, or throws a term it is the responsibility of the
    developer to handle this errors in his own module rule.
    
    Function rules on the other hand might raise a `FunctionClauseError`
    which does not necessarily mean negligence on the side of the developer
    but simply is a limitation of the provided function at hand.
    
    Spex will probably allow to configure the caught errors for function
    rules or turn this feature of completely.

--- spec/spex/rule/module_spec.exs
+++ spec/spex/rule/module_spec.exs
@@ -1,63 +1,77 @@
-# defmodule Spex.Rule.ModuleSpec do
-#   use ESpec, async: true
+defmodule Spex.Rule.ModuleSpec do
+  use ESpec, async: true
 
-#   defmodule NotNilRule do
-#     def evaluate(value) do
-#       not is_nil(value)
-#     end
-#   end
+  defmodule NotNilRule do
+    def evaluate(value) do
+      not is_nil(value)
+    end
+  end
 
-#   it_behaves_like Shared.IsRuleSpec,
-#     valid_rules: [
-#       NotNilRule
-#     ],
-#     invalid_rules: [
-#       :a,
-#       1,
-#       &is_list/1,
-#       Spex.Operator.all([])
-#     ]
+  it_behaves_like Shared.IsRuleSpec,
+    rule_type: Spex.Rule.Evaluable.Atom,
+    valid_rules: [
+      NotNilRule
+    ],
+    invalid_rules: [
+      1,
+      &is_list/1,
+      Spex.Operator.all([])
+    ]
 
-#   it_behaves_like Shared.EvaluateSpec,
-#     rule: NotNilRule,
-#     valid_values: [
-#       %{},
-#       :a,
-#       MapSet.new()
-#     ],
-#     invalid_values: [
-#       nil
-#     ]
+  it_behaves_like Shared.EvaluateSpec,
+    rule: NotNilRule,
+    valid_values: [
+      %{},
+      :a,
+      MapSet.new()
+    ],
+    invalid_values: [
+      nil
+    ]
 
-#   defmodule RaisingRule do
-#     def evaluate(_value) do
-#       raise "Ain't nobody got time for that!"
-#     end
-#   end
+  describe "an arbitrary atom" do
+    subject do: Spex.evaluate(:an_arbitrary_atom, :anything)
 
-#   it_behaves_like Shared.EvaluateSpec,
-#     rule: RaisingRule,
-#     valid_values: [],
-#     invalid_values: [
-#       1,
-#       :"2",
-#       "3",
-#       %{4 => 5},
-#       [6, 7]
-#     ]
+    it "should raise an UndefinedFunctionError" do
+      expect (&subject/0) |> to(raise_exception UndefinedFunctionError)
+    end
+  end
 
-#   defmodule ThrowingRule do
-#     def evaluate(value), do: throw(value)
-#   end
+  defmodule InvalidRule do
+    # No `evaluate/1`
+  end
 
-#   it_behaves_like Shared.EvaluateSpec,
-#     rule: ThrowingRule,
-#     valid_values: [],
-#     invalid_values: [
-#       1,
-#       :"2",
-#       "3",
-#       %{4 => 5},
-#       [6, 7]
-#     ]
-# end
+  describe "a raising module rule" do
+    subject do: Spex.evaluate(InvalidRule, :anything)
+
+    it "should raise an UndefinedFunctionError" do
+      expect (&subject/0) |> to(raise_exception UndefinedFunctionError)
+    end
+  end
+
+  defmodule RaisingRule do
+    def evaluate(_value) do
+      raise "Ain't nobody got time for that!"
+    end
+  end
+
+  describe "a raising module rule" do
+    subject do: Spex.evaluate(RaisingRule, :anything)
+
+    it "should raise an error" do
+      expect (&subject/0) |> to(raise_exception())
+    end
+  end
+
+  defmodule ThrowingRule do
+    def evaluate(value), do: throw(value)
+  end
+
+  describe "a throwing module rule" do
+    subject do: Spex.evaluate(ThrowingRule, :anything)
+
+    it "should throw something" do
+      expect (&subject/0) |> to(throw_term())
+    end
+  end
+end

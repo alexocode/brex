8813cc58e04fefaacd0ddb91ce88c3dd2cb729f2
Sascha Wolf <swolf.dev@gmail.com>
2020-10-27 14:11:09 +0100

    operator/aggregator: Add failing doctests for the expected behaviour

--- lib/brex/operator/aggregator.ex
+++ lib/brex/operator/aggregator.ex
@@ -10,13 +10,13 @@ defmodule Brex.Operator.Aggregator do
     iex> Brex.Operator.Aggregator.none?([])
     true
 
-    iex> Brex.Operator.Aggregator.none?([nil, nil])
+    iex> Brex.Operator.Aggregator.none?([false, false])
     true
 
-    iex> Brex.Operator.Aggregator.none?([1, 2, nil])
-    true
+    iex> Brex.Operator.Aggregator.none?([true, true, false])
+    false
 
-    iex> Brex.Operator.Aggregator.none?([1, 2, 3])
+    iex> Brex.Operator.Aggregator.none?([true, true, true])
     false
   """
   def none?([]), do: true
